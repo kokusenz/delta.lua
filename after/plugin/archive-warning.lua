d2afab42e57d400c80168e9887443ab56b8db3f4
agil <andrewsebing@gmail.com>
2026-06-11 16:18:12 -0400

    archive warning

---
+++ after/plugin/archive-warning.lua
@@ -0,0 +1,2 @@
+-- to disable this message if you want to use a version earlier than deltaview.nvim v0.4.0 (which requires delta.lua installed), target v0.1.4 of delta.lua with your plugin manager.
+vim.notify('[delta.lua] delta.lua has been archived, and the code has been merged into deltaview.nvim. Make sure you are using deltaview.nvim v0.4.0 or later, and uninstall delta.lua.', vim.log.levels.WARN)
